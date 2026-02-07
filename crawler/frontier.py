36cf485415baca61bd3986124c4e1ad13d816474
navid-atashbar <atashbar@uci.edu>
2026-02-07 07:00:12 +0000

    changed frontier py to not get sql error

--- crawler/frontier.py
+++ crawler/frontier.py
@@ -6,7 +6,13 @@ from queue import Queue, Empty
 
 from utils import get_logger, get_urlhash, normalize
 from scraper import is_valid
+_original_connect = sqlite3.connect
 
+def _patched_connect(*args, **kwargs):
+    kwargs['check_same_thread'] = False
+    return _original_connect(*args, **kwargs)
+
+sqlite3.connect = _patched_connect
 class Frontier(object):
     def __init__(self, config, restart):
         self.logger = get_logger("FRONTIER")
