81b3ae1be28cc99dc9d09c4c900cddb0985e9440
Navid Atashbar <atashbar@uci.edu>
2026-02-06 21:51:26 -0800

    Finisehd the helper that gets the data from the function

--- scraper.py
+++ scraper.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 
 #GLOBAL DATA VARIABLES
 urls_scrapped = set()
+num_of_each_subdomain = {}
+longest_page = {"url":"", "length":0}
+word_counter = {}
+stop_words = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves']
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -31,17 +35,38 @@ def extract_next_links(url, resp):
         #checks if the href value even stores something
         if linkers:
             #DATA COLLECTIONS
-
+            # helper_get_data(linkers, html_info)
             combined_link = urljoin(url, linkers) #combine the last url with the next url
             defragged_link = urldefrag(combined_link)[0] #returns tuple of the url and the fragment
             list_of_links.append(defragged_link)
     return list_of_links
-def helper_get_data(url, html_info):
 
+def helper_get_data(url, html_info):
     urls_scrapped.add(url)
-    #get text
+    parsed = urlparse(url)
+    sub_d = parsed.netloc
+    if sub_d in num_of_each_subdomain:
+        num_of_each_subdomain[sub_d] += 1
+    else:
+        num_of_each_subdomain[sub_d] = 1
+    #get_words
+    words = html_info.get_text().split()
     #count words
+    len_of_words = len(words)
+    if len_of_words > longest_page["length"]:
+        longest_page["length"] = len_of_words
+        longest_page["url"] = url
     #get longest url
+    for word in words:
+        stripped_word = word.lower().strip(".,?!;:")
+        if stripped_word not in stop_words:
+            if stripped_word in word_counter:
+                word_counter[stripped_word] += 1
+            else:
+                word_counter[stripped_word] = 1
+    #COUNT WORDS
+
+
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
